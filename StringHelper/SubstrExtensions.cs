cec30d3313a43cefe40cf368b397ae474e6cd40f
lzientek <152565@supinfo.com>
2014-12-09 15:37:40 +0000

    unused using removed

--- StringHelper/SubstrExtensions.cs
+++ StringHelper/SubstrExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LucasHelpers.StringHelper
+﻿namespace LucasHelpers.StringHelper
 {
     public static class SubstrExtensions
     {
